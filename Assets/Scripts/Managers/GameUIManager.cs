51a171c0e58007f4a1c5fc68f27d1e61084dc204
mineus64 <43474851+mineus64@users.noreply.github.com>
2022-05-15 06:44:18 -0700

    Added a GameUIManager
    - Added a Game UI Manager to manage the game UI
    - Added a basic ammo count display

---
+++ Assets/Scripts/Managers/GameUIManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+    #region Singleton
+
+
+
+    #endregion
+
+    #region Variables
+
+
+
+    #endregion
+
+    #region General Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region Specific Methods
+
+
+
+    #endregion
+}

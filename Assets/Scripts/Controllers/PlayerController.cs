67fd6be15fd49e2854ed6452e235efeba0c0b7a3
mineus64 <43474851+mineus64@users.noreply.github.com>
2022-05-14 18:05:10 -0700

    Added Player Movement
    - Added handling for player inputs and player object movement

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,13 +4,16 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : NetworkBehaviour
 {
     #region Variables
 
-
+    [Header("Object Components")]
+    [SerializeField] CharacterController characterController;
+    [SerializeField] PlayerInput playerInput;
 
     #endregion
 
@@ -18,10 +21,23 @@ public class PlayerController : NetworkBehaviour
 
     void Awake() 
     {
+        // Todo on all players
+        if (characterController == null) {
+            characterController = this.GetComponent<CharacterController>();
+        }
+
+        // Todo only if this player is the current client's player
         if (isLocalPlayer) {
 
         }
+
+        // Todo only if this player is NOT the current client's player
         else {
+
+            if (playerInput == null) {
+                playerInput = this.GetComponent<PlayerInput>();
+            }
+            Destroy(playerInput);
 
         }
     }
@@ -42,7 +58,19 @@ public class PlayerController : NetworkBehaviour
 
     #region Specific Methods
 
+    public void MoveForwardBack(InputAction.CallbackContext context) 
+    {
+        float moveAmt = context.ReadValue<float>();
 
+        characterController.Move(Vector3.forward * moveAmt * Time.deltaTime);
+    }
+
+    public void StrafeLeftRight(InputAction.CallbackContext context) 
+    {
+        float moveAmt = context.ReadValue<float>();
+
+        characterController.Move(Vector3.right * moveAmt * Time.deltaTime);
+    }
 
     #endregion
 }

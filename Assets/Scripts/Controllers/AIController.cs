580064a87c5481cae3d0d1a16682b4b2860c68b9
mineus64 <43474851+mineus64@users.noreply.github.com>
2022-05-15 13:30:49 -0700

    Added NavMesh

--- Assets/Scripts/Controllers/AIController.cs
+++ Assets/Scripts/Controllers/AIController.cs
@@ -5,13 +5,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(CharacterController))]
 public class AIController : NetworkBehaviour
 {
     #region Variables
 
-    
+    [Header("Object Components")]
+    [SerializeField] CharacterController characterController;
+    [SerializeField] GameObject weaponAnchor;
+
+    [Header("Movement Values")]
+    [SerializeField] Vector3 movement;
+    [SerializeField] float baseMoveSpeed;
+    [SerializeField] float moveSpeed;
+
+    [Header("Sound Values")]
+    [SerializeField] float footstepVolume;
+    [SerializeField] float stepLength = 1f;
+
+    [Header("Weapon Values")]
+    [SerializeField] WeaponSlot[] weaponsInBag;
+    [SerializeField] int currentWeapon = 0;
+    [SerializeField] GameObject currentWeaponObj;
+    [SerializeField] bool isFiring;
+
+    [Header("Health Values")]
+    [SerializeField] float currentHealth;
+    [SerializeField] float maxHealth;
 
     #endregion
 
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -13,8 +13,6 @@ public class PlayerController : NetworkBehaviour
 
     [Header("Variables")]
 
-    [SerializeField] float stepLength = 1f;
-
     [Header("Object Components")]
     [SerializeField] CharacterController characterController;
     [SerializeField] PlayerInput playerInput;
@@ -28,6 +26,7 @@ public class PlayerController : NetworkBehaviour
 
     [Header("Sound Values")]
     [SerializeField] float footstepVolume;
+    [SerializeField] float stepLength = 1f;
 
     [Header("Weapon Values")]
     [SerializeField] WeaponSlot[] weaponsInBag;

3f3e9ad9cd3a49061d4b1b5cde7e3b25bcf75a42
mineus64 <43474851+mineus64@users.noreply.github.com>
2022-05-14 17:44:30 -0700

    Added Input Actions
    - Added an Input Actions object to handle keyboard inputs
    - Added some basic movement actions

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,9 +1,20 @@
+// This script should be attached to the player prefab. It handles player inputs and controls.
+
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
+
+
+
+    #endregion
+
+    #region General Methods
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +26,12 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    #endregion
+
+    #region Specific Methods
+
+
+
+    #endregion
 }

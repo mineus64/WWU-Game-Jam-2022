ae1984c3a289d825e6fad7d64e7847dc2d10cffb
mineus64 <43474851+mineus64@users.noreply.github.com>
2022-06-07 21:18:46 -0700

    Update TimerController.cs
    - Added methods around creating, destroying, updating, etc. timers

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -1,5 +1,6 @@
 // This script is used to control timing-related stuff for Sonisight
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -38,7 +39,9 @@ public class TimerController : MonoBehaviour
     {
         foreach (Timer timer in updateTimers)
         {
-            timer.Tick(Time.deltaTime);
+            if (timer.time > 0) {
+                timer.Tick(Time.deltaTime);
+            }
         }
     }
 
@@ -46,7 +49,9 @@ public class TimerController : MonoBehaviour
     {
         foreach (Timer timer in fixedUpdateTimers)
         {
-            timer.Tick(Time.fixedDeltaTime);
+            if (timer.time > 0) {
+                timer.Tick(Time.fixedDeltaTime);
+            }
         }
     }
 
@@ -54,18 +59,91 @@ public class TimerController : MonoBehaviour
 
     #region Specific Methods
 
+    public Action Add(float time, bool type = true) 
+    {
+        Action output;
 
+        if (type == true) {
+            output = AddUpdate(time);
+        }
+        else {
+            output = AddFixedUpdate(time);
+        }
+
+        return output;
+    }
+
+    public Action AddUpdate(float time)
+    {
+        Timer newTimer = new Timer(updateTimers.Count, time);
+        updateTimers.Add(newTimer);
+        return newTimer.callbackAction;
+    }
+
+    public Action AddFixedUpdate(float time)
+    {
+        Timer newTimer = new Timer(fixedUpdateTimers.Count, time);
+        fixedUpdateTimers.Add(newTimer);
+        return newTimer.callbackAction;
+    }
+
+    // Might create a memory leak, look into this more if we get memory problems
+    // I'm not sure if GC will properly destroy this class instance
+    public void Remove(int id, bool type = true)
+    {
+        if (type == true) {
+            RemoveUpdate(id);
+        }
+        else {
+            RemoveFixedUpdate(id);
+        }
+    }
+
+    public void RemoveUpdate(int id)
+    {
+        Timer removeTimer = updateTimers[id];
+        updateTimers.Remove(removeTimer);
+    }
+
+    public void RemoveFixedUpdate(int id)
+    {
+        Timer removeTimer = fixedUpdateTimers[id];
+        fixedUpdateTimers.Remove(removeTimer);
+    }
+
+    public void Restart(int id, bool type = true) 
+    {
+        if (type == true) {
+            RestartUpdate(id);
+        }
+        else {
+            RestartFixedUpdate(id);
+        }
+    }
+
+    public void RestartUpdate(int id) 
+    {
+        updateTimers[id].Restart();
+    }
+
+    public void RestartFixedUpdate(int id) 
+    {
+        fixedUpdateTimers[id].Restart();
+    }
 
     #endregion
 }
 
 // The Timer is a data object that holds values for a single timer
-public class Timer
+public class Timer : MonoBehaviour
 {
     #region Variables
 
     public int id {get; private set;}
+    public float startTime {get; private set;}
     public float time {get; private set;}
+    public Action callbackAction {get; private set;}
+    public bool reuse {get; private set;}
 
     #endregion
 
@@ -82,18 +160,29 @@ public class Timer
         time = Mathf.Max(time - tickAmount, 0);
 
         if (time <= 0) {
+            callbackAction?.Invoke();
 
+            if (reuse == false) {
+                TimerController.current.Remove(id);
+            }
         }
+    }
+
+    public void Restart() 
+    {
+        time = startTime;
     }
 
     #endregion
 
     #region Constructors
 
-    public Timer(int id, float startTime) 
+    public Timer(int id, float startTime, bool reuse = false) 
     {
         this.id = id;
+        this.startTime = startTime;
         this.time = startTime;
+        this.reuse = reuse;
     }
 
     #endregion
